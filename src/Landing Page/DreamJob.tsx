a87139d78d1b07378e97e28acb1eee49ac0f7f50
ShreyanshJain105 <shreyanshjain1206@gmail.com>
2025-06-16 12:35:12 +0530

    In this commit i update the Home page and created new component for the home page and updated data

---
+++ src/Landing Page/DreamJob.tsx
@@ -0,0 +1,26 @@
+const DreamJob = () => {
+    return (
+        
+            <div className="flex items-center px-16 ">
+                <div className="flex flex-col w-[45%] gap-3">
+                    <div className="text-6xl font-bold leading-tight text-mine-shaft-100 [&>span]:text-bright-sun-400">Find your <span>dream </span><span>job </span>with us</div>
+                    <div >
+                        Good life begins with good company.
+                        Start exploring 1000+ jobs in 1 place
+                    </div>
+                </div>
+                <div className="w-[55%] flex items-center justify-center">
+                    <div className="w-[30rem]">
+                        <img 
+                            src="/Boy.png" 
+                            alt="Person looking for job opportunities" 
+                        />
+                    </div>
+                </div>
+                </div>
+           
+       
+    );
+}
+
+export default DreamJob;
