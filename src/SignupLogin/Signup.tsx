7b0691fec6c6fe1b47c34a378d61b924bf7fc132
ShreyanshJain105 <shreyanshjain1206@gmail.com>
2025-07-21 23:09:37 +0530

    This commit for joing our sign up details with backend

--- src/SignupLogin/Signup.tsx
+++ src/SignupLogin/Signup.tsx
@@ -2,43 +2,60 @@ import { PasswordInput, rem, TextInput, Checkbox,Anchor, Button, Radio, Group } 
 import { IconAt, IconLock } from "@tabler/icons-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { registerUser } from "../Services/UserService";
 
+const form={
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    accountType:"APPLICANT",
+}
 
 const Signup = () => {
      const [value, setValue] = useState('react');
 
+     const [data,setData]=useState(form);
+
+     const handleChange=(event:any)=>{
+        if(typeof(event)=="string")setData({...data,accountType:event});
+        else setData({...data,[event.target.name]:event.target.value})
+     }
+     const handleSubmit=()=>{
+        registerUser(data).then((res)=>{
+            console.log(res);
+        }).catch((err)=>console.log(err));
+     }
+
     return <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
         <div className="text-2xl font-semibold">Create Account</div>
-        <TextInput
+        <TextInput value={data.name} onChange={handleChange} name="name"
             label="Full Name"
             placeholder="Your Name "
             withAsterisk
         />
-        <TextInput
+        <TextInput value={data.email} onChange={handleChange} name="email"
             leftSection={<IconAt style={{ width: rem(16), height: rem(16) }} />}
             label="E-mail"
             withAsterisk
             placeholder="Your email"
         />
-        <PasswordInput withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
-        <PasswordInput withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm Password" />
-         <Radio.Group
-      value={value}
-      onChange={setValue}
-      name="favoriteFramework"
-      label="Select your favorite framework/library"
-      description="This is anonymous"
-      withAsterisk
+        <PasswordInput value={data.password} name="password" onChange={handleChange} withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
+        <PasswordInput value={data.confirmPassword} name="confirmPassword" onChange={handleChange} withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm Password" />
+         <Radio.Group 
+        value={data.accountType} 
+        onChange={handleChange}
+        name="favoriteFramework"
+        label="You're ?"
+        withAsterisk
     >
         <Group mt="xs">
-      <Radio value="react" label="React" />
-      <Radio value="svelte" label="Svelte" />
-      <Radio value="ng" label="Angular" />
-      <Radio value="vue" label="Vue" />
+      <Radio className="py-4 px-6 has-[:checked]:border-bright-sun-400 has-[:checked]:bg-bright-sun-400/5 border border-mine-shaft-800 rounded-lg " autoContrast value="APPLICANT" label="APPLICANT" />
+      <Radio className="py-4 px-6 has-[:checked]:border-bright-sun-400 has-[:checked]:bg-bright-sun-400/5 border border-mine-shaft-800 rounded-lg " autoContrast value="EMPLOYER" label="EMPLOYER" />
       </Group>
     </Radio.Group>
         <Checkbox autoContrast label={<>I accept {''}<Anchor>Terms & Conditions</Anchor></>}/>
-        <Button autoContrast variant="filled">Sign up</Button>
+        <Button onClick={handleSubmit} autoContrast variant="filled">Sign up</Button>
 
         <div className="mx-auto">Have an account ?<Link to="/login" className="text-bright-sun-400 hover:underline">Login</Link></div>
     </div>
