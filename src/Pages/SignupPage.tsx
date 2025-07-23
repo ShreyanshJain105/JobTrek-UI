560f27946b681135e7e7b46c0fd2607897da957a
ShreyanshJain105 <shreyanshjain1206@gmail.com>
2025-07-23 22:03:23 +0530

    Add back button to signup & login page component and add new functionalities

--- src/Pages/SignupPage.tsx
+++ src/Pages/SignupPage.tsx
@@ -1,12 +1,19 @@
-import { IconRouteSquare } from "@tabler/icons-react";
+import { IconArrowLeft, IconRouteSquare } from "@tabler/icons-react";
 import Signup from "../SignupLogin/Signup";
 import Login from "../SignupLogin/Login";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Button } from "@mantine/core";
 
 const SignupPage = () => {
     const location=useLocation();
+    const navigate = useNavigate();
     return (
-        <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] overflow-hidden ">
+        <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] overflow-hidden relative ">
+             <Button my="md" leftSection={<IconArrowLeft size={20}/> } 
+             color="brightSun.4" 
+             className="!absolute left-5  z-10"
+             onClick={()=>navigate("/")} variant="light" >Home</Button>
             <div className={`w-[100vw] transition-all ease-in-out duration-1000 h-[100vh] flex [&>*]:flex-shrink-0 ${location.pathname=='/signup'?'-translate-x-1/2':'translate-x-0'}`}>
                 <Login/>
                 <div className={`w-1/2 h-full transition-all ease-in-out duration-1000 ${location.pathname=="/signup"?"rounded-r-[200px]":"rounded-l-[200px]"}  bg-mine-shaft-900 flex items-center gap-5 justify-center flex-col `}>
